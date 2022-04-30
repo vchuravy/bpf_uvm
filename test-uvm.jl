c750fbf9b6d622af08346a0266edc131cfbf2900
Valentin Churavy <v.churavy@gmail.com>
2022-04-30 12:57:19 -0400

    Cleanup test-uvm.jl

--- test-uvm.jl
+++ test-uvm.jl
@@ -7,16 +7,32 @@
 using CUDA
 
 const PAGE_SIZE = 4096
-const N = 1
+const N = 1024
 
 buf = CuArray{UInt8, 1, CUDA.Mem.Unified}(undef, (N*PAGE_SIZE,))
+cpu_buf = Base.unsafe_wrap(Array{UInt8}, Base.unsafe_convert(Ptr{UInt8}, buf.storage.buffer), (N*PAGE_SIZE,), own=false)
 
-# # CUDA.Mem.prefetch(buf.storage.buffer)
+function prefetch_pages(buf)
+    CUDA.Mem.prefetch(buf.storage.buffer)
+end
 
-function fetch_page(buf)
-    buf[threadIdx().x * PAGE_SIZE]
+function fetch_pages_cpu(buf)
+    for i in eachindex(buf)
+        buf[i] += 1
+    end
+end
+
+function fetch_pages(buf)
+    buf[threadIdx().x * PAGE_SIZE] += 1
     return
 end
 
-@cuda threads=N fetch_page(buf)
+function fetch_pages_gpu(buf)
+    @cuda threads=N fetch_pages(buf)
+    synchronize()
+end
 
+fetch_pages_cpu(cpu_buf)
+prefetch_pages(buf)
+fetch_pages_cpu(cpu_buf)
+fetch_pages_gpu(buf)
